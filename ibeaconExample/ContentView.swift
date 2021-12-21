318af1a7540945f80d6d105ae88a2e77594945c3
Elijah Allen <elijah.a.allen@gmail.com>
2021-12-21 06:08:23 -0700

    Initial Commit

---
+++ ibeaconExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ibeaconExample
+//
+//  Created by Gove Allen on 12/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ibeaconExample/ibeaconExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibeaconExampleApp.swift
+//  ibeaconExample
+//
+//  Created by Gove Allen on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ibeaconExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
